2587bdedd39bdf08e7bcb3764e554f908b095a25
fleuryP <71771254+fleuryP@users.noreply.github.com>
2022-06-02 11:54:17 +0200

    Add files via upload

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,28 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./queries');
-var cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("./queries");
+var cors = require("cors");
 const app = express();
-const port = 8000
+const port = 8000;
 
 app.use(bodyParser.json());
 
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
-//Ici middleware sans route, donc appliqué à toutes les requêtes pour implémenter CORS 
+//Ici middleware sans route, donc appliqué à toutes les requêtes pour implémenter CORS
 app.use(cors());
 
 //La route de base, ici pour tester le fonctionnement du serveur
-app.get('/', (request, response) => {
-    response.json({ info: 'Bien connecté au serveur' })
-  })
+app.get("/", (request, response) => {
+  response.json({ info: "Bien connecté au serveur" });
+});
 
-app.get('/data/:id', db.getData);
+//Route de récupération des données d'une personne, via l'id passé en paramètre
+app.get("/data/:id", db.getData);
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
--- src/backend/queries.js
+++ src/backend/queries.js
@@ -1,13 +1,12 @@
 const { response } = require("express");
-
 const Pool = require("pg").Pool;
 
 //Connexion à la db
 const pool = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "", // to complete
-  password: "", // to complete
+  database: "TER_db_3B",
+  password: "admin",
   port: 5432,
 });
 
@@ -23,22 +22,30 @@ let responseJSON = {}; //L'objet qu'on va alimenter avec les résultats des requ
  */
 async function getData(req, response) {
   console.log("-------------------------------------------------------------");
-  console.log("---------------ARRIVEE DANS LA FONCTION GETDATA---------------");
+  console.log("---------------ARRIVEE DANS LA FONCTION GETDATA--------------");
   console.log("-------------------------------------------------------------");
 
   let id = parseInt(req.params.id); //On récupère l'id de la personne dans la requete envoyée au serveur
 
-  //On attend la fin de chaque fonction requete pour tout bien récupérer avant de renvoyer la réponse
-  await getResidential(id);
-  await getFamilial(id);
-  await getProfessional(id);
-  await getLeisure(id);
+  if (id < 5000 || id > 9904 || isNaN(id)) {
+    console.log("User input invalide");
+    response.status(400).json({
+      error: "InvalidUserInput",
+      message: "Identifiant entré invalide",
+    });
+  } else {
+    //On attend la fin de chaque fonction requete pour tout bien récupérer avant de renvoyer la réponse
+    await getResidential(id);
+    await getFamilial(id);
+    await getProfessional(id);
+    await getLeisure(id);
 
-  response.status(200).json(responseJSON); //On renvoie l'objet qu'on a construit avec les requetes
+    response.status(200).json(responseJSON); //On renvoie l'objet qu'on a construit avec les requetes
 
-  console.log("--------------------------------------------");
-  console.log("------------FIN DE LA REQUETE-------------");
-  console.log("--------------------------------------------");
+    console.log("--------------------------------------------");
+    console.log("------------FIN DE LA REQUETE---------------");
+    console.log("--------------------------------------------");
+  }
 }
 
 /**
@@ -50,15 +57,10 @@ async function getResidential(id) {
   console.log("On passe dans la fonction rési, id vaut : " + id);
 
   let requeteRes =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT fk_ref_loc as loc, 'habitat' as type_event, annee as annee FROM residential_event WHERE personne = $1 UNION SELECT fk_ref_loc as loc, 'etudes' as type_event, annee as annee FROM professionnal_event WHERE personne = $1 AND type_event='Etude' UNION SELECT fk_ref_loc as loc,  'naissance' as type_event, annee as annee FROM familial_event WHERE type_event='Naissance ego' AND personne = $1 ORDER by annee) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT fk_ref_loc as loc, 'habitat' as type_event, annee as annee FROM residential_event WHERE personne = $1 UNION SELECT fk_ref_loc as loc, 'etudes' as type_event, annee as annee FROM professionnal_event WHERE personne = $1 AND type_event='Etude' UNION SELECT fk_ref_loc as loc,  'naissance' as type_event, annee as annee FROM familial_event WHERE type_event='Naissance ego' AND personne = $1 ORDER by annee) as requete WHERE requete.loc = pk_ref_loc";
 
-  //Entrée dans un try/catch pour gérer les éventuelles erreurs de requête
-  try {
-    let colonnes = (await pool.query(requeteRes, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.residential = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
+  //Appel de la fonction requete avec seulement un id principal
+  await request(requeteRes, id, "residential", null);
 }
 
 /**
@@ -75,46 +77,19 @@ async function getFamilial(id) {
   //On fait une requete pour chaque sous-dimension du familial, ce qui va trier facilement et éviter les problèmes de rang similaires
   //Et faciliter le travail de traitement dans le frontend
   let requeteMariage =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT rang, annee, fk_ref_loc as loc FROM familial_event WHERE type_event='Mariage' AND personne = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT rang, annee, fk_ref_loc as loc FROM familial_event WHERE type_event='Mariage' AND personne = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
   let requeteEnfants =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT rang, annee, fk_ref_loc as loc FROM enfant WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT rang, annee, type_lieu, fk_ref_loc as loc FROM enfant WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
   let requeteParents =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT rang, annee, type_lieu, fk_ref_loc as loc FROM parent WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT rang, annee, type_lieu, fk_ref_loc as loc FROM parent WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
   let requeteConjoint =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT annee, type_lieu, fk_ref_loc as loc FROM conjoint WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT annee, type_lieu, fk_ref_loc as loc FROM conjoint WHERE fk_personne_id = $1 ORDER BY annee) as requete WHERE requete.loc = pk_ref_loc";
 
-  //Entrée dans un try/catch pour gérer les éventuelles erreurs de requête
-  //Requete concernant le mariage
-  try {
-    let colonnes = (await pool.query(requeteMariage, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.familial.mariage = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
-
-  //Requete concernant les enfants
-  try {
-    let colonnes = (await pool.query(requeteEnfants, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.familial.enfants = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
-
-  //Requete concernant les parents
-  try {
-    let colonnes = (await pool.query(requeteParents, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.familial.parents = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
-
-  //Requete concernant le conjoint
-  try {
-    let colonnes = (await pool.query(requeteConjoint, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.familial.conjoint = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
+  //Appel de la fonction de requete pour chaque sous-objet de la dimension familiale, donc on passe également un idSecondaire (au lieu de null ailleurs)
+  await request(requeteMariage, id, "familial", "mariage");
+  await request(requeteEnfants, id, "familial", "children");
+  await request(requeteParents, id, "familial", "parents");
+  await request(requeteConjoint, id, "familial", "partner");
 }
 
 /** Fonction qui récupère les informations pro dans la db et les place dans l'objet json réponse, avec l'id 'pro'
@@ -125,14 +100,10 @@ async function getProfessional(id) {
   console.log("On passe dans la fonction pro, id vaut : " + id);
 
   let requetePro =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT rang, date_debut, date_fin, fk_ref_loc as loc FROM professionnal_episode WHERE type_episode='Emploi' AND personne = $1 ORDER BY date_debut) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT rang, date_debut, date_fin, fk_ref_loc as loc FROM professionnal_episode WHERE type_episode='Emploi' AND personne = $1 ORDER BY date_debut) as requete WHERE requete.loc = pk_ref_loc";
 
-  try {
-    let colonnes = (await pool.query(requetePro, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.pro = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
-  }
+  //Appel de la fonction requete avec seulement un id principal
+  await request(requetePro, id, "pro", null);
 }
 
 /** Récupère les informations des voyages d'une personne donnée et les place dans la réponse JSON avec l'id 'leisure'
@@ -142,13 +113,34 @@ async function getProfessional(id) {
 async function getLeisure(id) {
   console.log("On passe dans la fonction voyages, id vaut : " + id);
   let requeteLeisure =
-    "SELECT requete.*, latitude, longitude FROM localisation, (SELECT rang, date_debut, date_fin, type_episode, fk_ref_loc as loc FROM leisure_episode WHERE personne = $1 ORDER BY date_debut) as requete WHERE requete.loc = pk_ref_loc";
+    "SELECT requete.*, latitude, longitude, pays FROM localisation, (SELECT rang, date_debut, date_fin, type_episode, fk_ref_loc as loc FROM leisure_episode WHERE personne = $1 ORDER BY date_debut) as requete WHERE requete.loc = pk_ref_loc";
 
-  try {
-    let colonnes = (await pool.query(requeteLeisure, [id])).rows; //On attend la fin de la requete et on met les rows dans une fonction
-    responseJSON.leisure = colonnes; //On place ces colonnes dans l'objet réponse avec le bon id
-  } catch (error) {
-    throw error;
+  //Appel de la fonction requete avec seulement un id principal
+  await request(requeteLeisure, id, "leisure", null);
+}
+
+/**
+ * Fonction de factorisation qui va effectuer la requete SQL auprès de la db, donc qu'on va appeler pour chaque requete dans les fonctions au-dessus
+ *
+ * @param {String} requete la requete postGre a envoyer à la db
+ * @param {*} idPersonne l'id de la personne à passer dans la requete
+ * @param {String} idPrincipalReponse id auquel on va mettre les données dans la réponse
+ * @param {String} idSecondaireReponse si besoin l'id du sous objet (utile dans familial notamment)
+ */
+async function request(
+  requete,
+  idPersonne,
+  idPrincipalReponse,
+  idSecondaireReponse
+) {
+  let colonnes = (await pool.query(requete, [idPersonne])).rows; //On attend la fin de la requete et on met les rows dans une fonction
+
+  if (idSecondaireReponse == null) {
+    //Si on a seulement un id et pas de sous-id
+    responseJSON[idPrincipalReponse] = colonnes; //On place ces colonnes dans l'objet avec seulement l'id
+  } else {
+    //Dans ce cas on doit placer les réponses avec le sous-id, l'id principal aura été initialisé avant
+    responseJSON[idPrincipalReponse][idSecondaireReponse] = colonnes; //On place ces colonnes dans l'objet avec l'id principal + l'id secondaire
   }
 }
 
